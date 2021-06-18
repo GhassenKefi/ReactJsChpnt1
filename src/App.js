f1d54560cb2e1f7d4ab3cda4d6eebf9ae3bc9d0a
ghassen kefi <ghassen.kefi.dev@gmail.com>
2021-06-18 11:54:43 +0200

    end of checkpoint

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
-import './App.css';
+import "./App.css";
+import { Form,Button } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+    <div className="content">
+    <Form>
+      <Form.Group controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
     </div>
+    <div className="footer">
+    <div class="zoom"> 
+      <a href="https://gomycode.com/">
+        GOMYCODE
+      </a>
+      </div>
+    </div>
+    </div>
+
   );
 }
 
